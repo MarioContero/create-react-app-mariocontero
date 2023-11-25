00c384291e1cfadee901513dcf92be309ff970b0
MarioContero <119126946+MarioContero@users.noreply.github.com>
2023-11-24 20:41:01 -0500

    Update App.js
    cambio a PUCE Mario Contero

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.puce.edu.ec/"
           target="_blank"
           rel="noopener noreferrer"
         >
