078b5ad86535567c3fcdee4d56ed417194fd466a
MarioContero <macontero@puce.edu.ec>
2023-11-26 20:48:27 -0500

    prueba

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
+import newBackground from './newBackground.jpg'; // Asegúrate de tener esta imagen en tu directorio
+import newImage from './newImage.jpg'; // Asegúrate de tener esta imagen en tu directorio
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${newBackground})` }}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={newImage} alt="new" /> {/* Aquí se agrega la nueva imagen */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
